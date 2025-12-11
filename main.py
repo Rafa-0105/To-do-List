bd13009b7df0331fe5b198af83c252d8c65de1e6
Rafa-0105 <rafaelgambinrocha@hotmail.com>
2025-12-10 23:16:42 -0300

    Refactor: Change library Flask Restx

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from routes.get.method_get import app
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- routes/get/method_get.py
+++ routes/get/method_get.py
@@ -1,10 +1,30 @@
-from flask import Flask, jsonify
-
+from flask import Flask
+from flask_restx import Api, Resource
 from DB.conection import conn
+from sqlalchemy import text
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/teste", methods=['GET'])
-def api_get():
-    get_db = conn()
-    return jsonify("Conection on DataBase sucess!"), 200
+@api.route("/teste", methods=['GET'])
+class api_get(Resource):
+    def get(self):
+        engine = conn()
+        if not engine:
+            return {"error": "Falha na conexão"}, 500
+            
+        try:
+            with engine.connect() as connection:
+                result = connection.execute(text("SELECT * FROM teste"))
+                results = result.fetchall()
+                
+                data = [{"name": row[0], "password": row[1]} for row in results]
+                
+                return {
+                    "success": True,
+                    "count": len(data),
+                    "data": data
+                }, 200
+                
+        except Exception as e:
+            return {"error": str(e)}, 500
